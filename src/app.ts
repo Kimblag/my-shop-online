5c0eb307f2b0166453a63ce762496d9738fc264c
Kimberly Blandon <kimberly.blandong@gmail.com>
2022-07-02 17:57:05 -0300

    added cors policies

--- src/app.ts
+++ src/app.ts
@@ -11,11 +11,28 @@ const app: Application = express()
 // Settings
 app.set('port', process.env.PORT || 3001)
 
+let ALLOWED_ORIGINS = [
+    "https://my-shop-online.vercel.app",
+    "http://localhost:3000",
+  ];
 //middlewares
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.set("trust proxy", 1);
+app.use((req, res, next) => {
+    let origin = req.headers.origin as string;
+      let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
+    res.header("Access-Control-Allow-Origin", theOrigin); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 app.use(passport.initialize())
 passport.use(passportMiddleware)
 
